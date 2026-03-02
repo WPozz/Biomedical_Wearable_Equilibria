686b4aec337a3e7c4feac51b549977e6ca480a0b
William Pozzolini <156839394+WPozz@users.noreply.github.com>
2026-03-02 13:53:42 +0100

    Implement function to print random integers
    Added a function to print random integers.

--- Esercizio_1.dart
+++ Esercizio_1.dart
@@ -6,3 +6,13 @@ void smain(n){
   for (var i = 0; i < n; i++){
     print(pow(i, 4));}
 }
+
+// n the main function, given an integer number n, instantiate a list of n random integers, with possible maximum 
+//value 10. Then, for each element of the list, print it multiplied by 2.
+
+void drain() {
+  var rng = Random();
+  for (var n = 0; n < 5; n++) {
+    print(rng.nextInt(10));
+  }
+}
